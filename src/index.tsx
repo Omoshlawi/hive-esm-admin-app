a2e0e9fa7c6e3a98717c19301e8fef519966b1ab
Laurent Ouma <lawiomosh3@gmail.com>
2025-06-05 14:04:54 +0300

    Add MyOrganizationsPage component and update routing in setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,59 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import type { PiletApi } from "@hive/esm-shell-app";
 import { EXTENSION_SLOTS } from "./utils/constants";
+import { NavLink } from "@mantine/core";
+import { IconChevronRight } from "@tabler/icons-react";
+import { HeaderLink, withUserAccess } from "@hive/esm-core-components";
+import MyOrganizationsPage from "./pages/MyOrganizationsPage";
 
 const Page = React.lazy(() => import("./Page"));
 
 export function setup(app: PiletApi) {
-  const { getSession, exitOrganizationContext, switchOrganizationContext } =
-    app;
+  const {
+    getSession,
+    exitOrganizationContext,
+    switchOrganizationContext,
+    session,
+    launchWorkspace,
+  } = app;
 
   app.registerPage("/", Page, { layout: "dashboard" });
+  app.registerPage(
+    "/dashboard/my-organizations",
+    withUserAccess(
+      () => (
+        <MyOrganizationsPage
+          session={session}
+          exitOrganizationContext={exitOrganizationContext}
+          switchOrganizationContext={switchOrganizationContext}
+          launchWorkspace={launchWorkspace}
+        />
+      ),
+
+      {
+        getSession,
+        isAuthenticated: (session) => session.isAuthenticated,
+        requiresAuth: true,
+      }
+    ),
+    {
+      layout: "dashboard",
+    }
+  );
   app.registerExtension(
     EXTENSION_SLOTS.DASHBOARD_QUICK_ACTION_SLOT,
     React.lazy(() => import("./components/AddOrganizationQucikAction"))
   );
-  app.registerMenu(() => <Link to="/page">Page</Link>, { type: "header" });
+  app.registerMenu(
+    ({ onClose }) => (
+      <HeaderLink
+        to="/dashboard/my-organizations"
+        label="My Organizations"
+        onClose={onClose ?? (() => {})}
+      />
+    ),
+    {
+      type: "admin",
+    }
+  );
 }
---
+++ src/pages/MyOrganizationsPage.tsx
@@ -0,0 +1,126 @@
+import { PiletApi } from "@hive/esm-shell-app";
+import React, { FC, useMemo } from "react";
+import { useMyOrganizationApi, useMyOrganizations } from "../hooks";
+import { Organization } from "../types";
+import { showNotification } from "@mantine/notifications";
+import { openConfirmModal } from "@mantine/modals";
+import {
+  EmptyState,
+  ErrorState,
+  TableContainer,
+  TablerIcon,
+  When,
+} from "@hive/esm-core-components";
+import {
+  ActionIcon,
+  Loader,
+  Menu,
+  Table,
+  TableData,
+  Text,
+} from "@mantine/core";
+import { stat } from "fs";
+import OrgaznizationForm from "../forms/OrgaznizationForm";
+
+type MyOrganizationsPageProps = Pick<
+  PiletApi,
+  | "switchOrganizationContext"
+  | "exitOrganizationContext"
+  | "session"
+  | "launchWorkspace"
+> & {};
+
+const MyOrganizationsPage: FC<MyOrganizationsPageProps> = ({
+  switchOrganizationContext,
+  exitOrganizationContext,
+  session,
+  launchWorkspace,
+}) => {
+  const state = useMyOrganizations(session.user?.id);
+  const title = "My Organizations";
+  const handleUpdate = (organization: Organization) => {
+    const dispose = launchWorkspace(
+      <OrgaznizationForm
+        organization={organization}
+        // onSuccess={() => dispose()}
+        onCloseWorkspace={() => dispose()}
+      />,
+      { title: "Update amenity" }
+    );
+  };
+
+  const handleDelete = (organization: Organization) => {
+    openConfirmModal({
+      title: "Delete Organization",
+      children: (
+        <Text>
+          Are you sure you want to delete this organization.This action is
+          destructive and will delete all data related to organization
+        </Text>
+      ),
+      labels: { confirm: "Delete Organization", cancel: "No don't delete it" },
+      confirmProps: { color: "red" },
+      centered: true,
+      onConfirm() {
+        // TODO Implement delete
+      },
+    });
+  };
+  const tableData = useMemo<TableData>(
+    () => ({
+      head: ["Name", "Description", "CreatedAt", "Actions"],
+      body: state?.organizationsMemberships.map((docType) => [
+        docType.organization.name,
+        docType.organization.description,
+        new Date(docType.createdAt).toDateString(),
+        <Menu shadow="md" width={200}>
+          <Menu.Target>
+            <ActionIcon variant="outline" aria-label="Settings">
+              <TablerIcon
+                name="dotsVertical"
+                style={{ width: "70%", height: "70%" }}
+                stroke={1.5}
+              />
+            </ActionIcon>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item
+              leftSection={<TablerIcon name="edit" size={14} />}
+              color="green"
+              onClick={() => handleUpdate(docType.organization)}
+            >
+              Edit
+            </Menu.Item>
+            <Menu.Item
+              leftSection={<TablerIcon name="trash" size={14} />}
+              color="red"
+              onClick={() => handleDelete(docType.organization)}
+            >
+              Delete
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>,
+      ]),
+    }),
+    [state.organizationsMemberships]
+  );
+  return (
+    <div>
+      <When
+        asyncState={{ ...state, data: state.organizationsMemberships }}
+        loading={() => <Loader />}
+        error={(e) => <ErrorState error={e} headerTitle={title} />}
+        success={(data) => {
+          if (!data.length) return <EmptyState headerTitle={title} />;
+          return (
+            <TableContainer title={title}>
+              <Table data={tableData} />
+            </TableContainer>
+          );
+        }}
+      />
+    </div>
+  );
+};
+
+export default MyOrganizationsPage;
